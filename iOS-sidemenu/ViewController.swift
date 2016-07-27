53c4c9b123ec1744ca55f81c3e15b990fb75d94e
Paul Ku <justlove0714@hotmail.com>
2016-07-27 21:22:27 +0800

    project init.

--- iOS-sidemenu/ViewController.swift
+++ iOS-sidemenu/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("This is a  init project of side menu.")
     }
 
     override func didReceiveMemoryWarning() {
