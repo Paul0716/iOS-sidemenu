70449f62795a5fc7810533e4512aba105fe3f7bf
Paul Ku <justlove0714@hotmail.com>
2016-07-27 21:16:28 +0800

    Initial Commit

---
+++ iOS-sidemenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-sidemenu
+//
+//  Created by Paul Ku on 2016/7/27.
+//  Copyright © 2016年 Paul Ku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
